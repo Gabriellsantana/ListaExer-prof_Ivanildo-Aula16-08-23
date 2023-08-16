1e07886eba77607e9f7bac8e2b47f28b484befc9
Gabriel <115082578+Gabriellsantana@users.noreply.github.com>
2023-08-16 15:14:35 -0300

    adicionado questão 5

--- listaexrc16.py
+++ listaexrc16.py
@@ -44,3 +44,45 @@ for i in range(5):
  lista4["tel"] = input("digite seu tel")
  i =i+1
 print(lista4)
+
+
+'''
+5 Considere um sistema onde os dados são armazenados em dicionários.
+Nesse sistema existe um dicionario principal e o dicionário de backup.
+Cada vez que o dicionário principal atinge tamanho 5, ele imprime os
+dados na tela e apaga o seu conteúdo. Crie um programa que insira dados
+em um dicionário, realizando o backup a cada dado e excluindo os dados
+do dicionário principal quando ele atingir tamanho 5.
+'''
+def realizar_backup(dicionario_principal, dicionario_backup):
+    dicionario_backup.update(dicionario_principal)
+    dicionario_principal.clear()
+
+def main():
+    dicionario_principal = {}
+    dicionario_backup = {}
+    contador = 1
+
+    while True:
+        nome = input("Digite o nome : ")
+        idade = int(input("Digite a idade da pessoa: "))
+        cpf = input("Digite o CPF : ")
+        pessoa = {'nome': nome, 'idade': idade, 'cpf': cpf}
+        dicionario_principal[cpf] = pessoa
+
+        if len(dicionario_principal) >= 5:
+            print(f"\nRealizando backup {contador}")
+            realizar_backup(dicionario_principal, dicionario_backup)
+            contador += 1
+
+        print("\nDicionário Principal:")
+        for cpf, pessoa in dicionario_principal.items():
+            print(f"CPF: {cpf}, Pessoa: {pessoa}")
+
+        print("\nDicionário de Backup:")
+        for cpf, pessoa in dicionario_backup.items():
+            print(f"CPF: {cpf}, Pessoa: {pessoa}")
+
+if __name__ == "__main__":
+    main()
+
